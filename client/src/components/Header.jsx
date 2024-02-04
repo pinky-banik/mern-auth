86a16fa353d1f77b17962deaf3540877d357bb93
pinky-banik <pinky.rani.banik@gmail.com>
2024-02-04 13:55:05 +0600

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+import { Link } from "react-router-dom";
+
+const Header = () => {
+    return (
+        <div className="bg-slate-200">
+            <div className="flex justify-between items-center mx-auto p-5">
+                <h1 className="font-bold">Auth App</h1>
+                <ul className="flex gap-5">
+                    <Link to='/'>Home</Link>
+                    <Link to='/about'>About</Link>
+                    <Link to='/sign-in'>Sign In</Link>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
