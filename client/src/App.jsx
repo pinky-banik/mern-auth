babfa1fe372a326fa03a76a61797809a5edb49f1
pinky-banik <pinky.rani.banik@gmail.com>
2024-02-04 13:40:16 +0600

    installing react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from "./pages/SignUp";
+import Profile from './pages/Profile';
 
 
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl text-red-700">hello</h1>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/sign-in" element={<SignIn/>}/>
+          <Route path="/sign-up" element={<SignUp/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+      </Routes>
     </>
   )
 }
