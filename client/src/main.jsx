1663f7e0f12a0174333cacd2c67c946ef9bd4249
pinky-banik <pinky.rani.banik@gmail.com>
2024-02-06 11:08:46 +0600

    use redux persistor to store data in localstorage

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,14 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
     <BrowserRouter>
     <App />
-    </BrowserRouter>
+        </BrowserRouter>
+        </PersistGate>
     </Provider>
   </React.StrictMode>
     
